3b8b954478aabf978063ac741d93eb2b234ee2c7
DELL <DELL@DESKTOP-AECTL04>
2020-03-06 20:06:53 +0530

    sixth commit

---
+++ akhila-collections1/src/main/java/AkhilaTask_6/akhila_collections1/App.java
@@ -0,0 +1,13 @@
+package AkhilaTask_6.akhila_collections1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
